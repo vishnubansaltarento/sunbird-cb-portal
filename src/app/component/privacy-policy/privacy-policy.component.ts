6e58d78ef787cbb5cd6261f1acb473cb0042cfe0
ManeeshaShibu <maneesha.shibu2@gmail.com>
2024-04-12 16:43:12 +0530

    added multilingual code

--- src/app/component/privacy-policy/privacy-policy.component.ts
+++ src/app/component/privacy-policy/privacy-policy.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
-  styleUrls: ['./privacy-policy.component.scss'],
+  styleUrls: ['./privacy-policy.component.scss']
 })
 export class PrivacyPolicyComponent implements OnInit {
 
-  constructor() {
+  constructor(private translate: TranslateService) {
+    if (localStorage.getItem('websiteLanguage')) {
+      this.translate.setDefaultLang('en')
+      const lang = localStorage.getItem('websiteLanguage')!
+      this.translate.use(lang)
+    }
   }
 
   ngOnInit(): void {
+    
+  }  
 
-  }
-
+ changeLanguage(language: string) {
+    this.translate.use(language);
+ }
 }
