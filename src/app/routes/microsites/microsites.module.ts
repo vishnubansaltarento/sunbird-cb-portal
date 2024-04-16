cbbbc5db5c268df9ec20b0026898fb5400c977cb
Christyfernandes <christyabcd.05@gmail.com>
2024-04-16 11:40:37 +0530

    Task #0000: initial code for microsites

---
+++ src/app/routes/microsites/microsites.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MicrosotesComponent } from './microsotes.component';
+import { MatIconModule } from '@angular/material';
+import { ContentStripWithTabsModule, SlidersModule } from '@sunbird-cb/collection/src/public-api';
+
+
+
+@NgModule({
+  declarations: [MicrosotesComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    SlidersModule,
+    ContentStripWithTabsModule,
+  ]
+})
+export class MicrositesModule { }
