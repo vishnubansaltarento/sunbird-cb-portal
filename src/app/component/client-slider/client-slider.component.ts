2d35cdaf7d89af6b95d36c592facbc39126dbc4d
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-05-08 18:39:07 +0530

    Providers list API integration

--- src/app/component/client-slider/client-slider.component.ts
+++ src/app/component/client-slider/client-slider.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
+import { BrowseProviderService } from '@ws/app/src/lib/routes/browse-by-provider/services/browse-provider.service'
 
 @Component({
   selector: 'ws-client-slider',
@@ -7,10 +8,24 @@ import { TranslateService } from '@ngx-translate/core'
   styleUrls: ['./client-slider.component.scss'],
 })
 export class ClientSliderComponent implements OnInit {
-  @Input() clientList: any
+  // @Input() clientList: any
   clients: any
   noClients = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  constructor(private translate: TranslateService) {
+  limit = 20
+  getAllProvidersReq = {
+    request: {
+      filters: {
+        isCbp: true,
+      },
+      sort_by: {
+        orgName: 'asc',
+      },
+      query: '',
+      limit: this.limit,
+      offset: 0,
+    },
+  }
+  constructor(private translate: TranslateService, private browseProviderService: BrowseProviderService) {
     if (localStorage.getItem('websiteLanguage')) {
       this.translate.setDefaultLang('en')
       const lang = localStorage.getItem('websiteLanguage')!
@@ -19,6 +34,12 @@ export class ClientSliderComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.clients = this.clientList
+    // this.clients = this.clientList
+
+    this.browseProviderService.fetchAllProviders(this.getAllProvidersReq).subscribe((result: any) => {
+      if (result) {
+        this.clients = result
+      }
+    })
   }
 }
