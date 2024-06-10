15820f0919d51dc6354e33bf8ee8c2294e9482f8
Nandini Achutha <nandini.achutha@tarento.com>
2024-06-10 16:43:34 +0530

    Fixed 5184

--- project/ws/app/src/lib/routes/profile-v2/routes/profile-view/profile-view.component.ts
+++ project/ws/app/src/lib/routes/profile-v2/routes/profile-view/profile-view.component.ts
@@ -677,7 +677,7 @@ export class ProfileViewComponent implements OnInit, AfterViewInit, OnDestroy {
           'personalDetails': {},
           'employmentDetails': {
             'employeeCode': this.otherDetailsForm.value['employeeCode'],
-            'pincode': this.otherDetailsForm.value['pincode'],
+            'pinCode': this.otherDetailsForm.value['pincode'],
             'mobile': this.otherDetailsForm.value['mobile'],
           },
         },
