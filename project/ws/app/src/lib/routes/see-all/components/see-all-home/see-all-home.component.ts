96268766e609f15149b8cdcf8d0ececfd3a9196e
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2024-07-02 13:36:16 +0530

    changes in collapse

--- project/ws/app/src/lib/routes/see-all/components/see-all-home/see-all-home.component.ts
+++ project/ws/app/src/lib/routes/see-all/components/see-all-home/see-all-home.component.ts
@@ -46,7 +46,7 @@ export class SeeAllHomeComponent implements OnInit, OnDestroy {
       this.tabCompleted = (res.tab) ? res.tab : ''
       this.tabSelected = (res.tabSelected) ? res.tabSelected : ''
     })
-
+    debugger
     const configData = await this.seeAllSvc.getSeeAllConfigJson().catch(_error => {})
     configData.homeStrips.forEach((ele: any) => {
       if (ele && ele.strips.length > 0) {
@@ -57,6 +57,17 @@ export class SeeAllHomeComponent implements OnInit, OnDestroy {
         })
       }
     })
+    if(!this.seeAllPageConfig) {
+      configData.assessmentData.forEach((ele: any) => {
+        if (ele && ele.strips.length > 0) {
+          ele.strips.forEach((subEle: any) => {
+            if (subEle.key === this.keyData) {
+              this.seeAllPageConfig = subEle
+            }
+          })
+        }
+      })
+    }
     if (
       this.tabSelected &&
       this.seeAllPageConfig.tabs &&
