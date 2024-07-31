fb07bde697e449e848683d6dbce5106542453d4a
nitinraj-tarento <nitin.raj@tarento.com>
2024-07-31 17:58:23 +0530

    lint error fixed

--- library/ws-widget/collection/src/lib/_common/content-toc/pipes/truncate.pipe.ts
+++ library/ws-widget/collection/src/lib/_common/content-toc/pipes/truncate.pipe.ts
@@ -1,14 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({
-  name: 'truncate'
+  name: 'truncate',
 })
 export class TruncatePipe implements PipeTransform {
   transform(value: string, limit: number): string {
     if (!value) {
-      return '';
+      return ''
     }
-    const words = value.split(' ');
-    return words.length > limit ? words.slice(0, limit).join(' ') + '...' : value;
+    const words = value.split(' ')
+    return words.length > limit ? (`${words.slice(0, limit).join(' ')}...`): value
   }
 }
